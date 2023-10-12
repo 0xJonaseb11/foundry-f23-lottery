7208e0b4bbc77497d4d7fd83084fe6c46ab853f9
jonas-sebera <sebejaz99@gmail.com>
2023-10-12 10:13:25 +0200

    interactions init

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -1,3 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
+import { Script, Console } from "forge-std/Script.sol";
+import { Solmate } from "@solmate/";
+import { OpenZeppelin } from "@oppenzeppelin/contracts/src/v0.8/";
+
+
+contract Interactions is Script {
+
+}
+
